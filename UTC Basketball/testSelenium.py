f5545d879915d4bfe551ef87c51b7d065f73cabf
Max Doumitt <grengren34@gmail.com>
2023-02-02 18:16:57 -0500

    Commit 1
    Starting to get a grasp on selenium and how I will go about automating the data scraping on the first website

--- UTC Basketball/testSelenium.py
+++ UTC Basketball/testSelenium.py
@@ -1,15 +1,36 @@
+#Import Usernames and Passwords
+import logins
 #Selenium Imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+#Global variables and functions
 #Sets Chrome as web browser (Install Chrome & Chromium)
 driver = webdriver.Chrome()
-#Goes to the ShotQuality Website
-driver.get("https://shotquality.com/team/college_mens/2023/Chattanooga")
-#Checks if "ShotQuality" is in the title to varify we are in the right place
-assert "ShotQuality" in driver.title 
-#Finds the Class for the rankList on the webpage and selects it as elem
-elem = driver.find_element_by_class_name("rankList")
-#screenshots elem and names the file 'foo.png'
-elem.screenshot('foo.png')
+#Let the page load for a few seconds
+def let_load(x):
+    driver.implicitly_wait(x) 
+
+
+#ShotQuality Screenshot Function
+def shot_quality():
+    #Goes to the ShotQuality Website
+    driver.get("https://shotquality.com/login")
+    #Checks if "ShotQuality" is in the title to varify we are in the right place
+    assert "ShotQuality" in driver.title 
+    #Load page
+    let_load(150)
+    #Find the locations of necessary LOGIN page items
+    email_input = driver.find_element(By.XPATH, "//input[@type=\"email\"]") #email field
+    pass_input = driver.find_element(By.XPATH, "//input[@type=\"password\"]") #password field
+    #writes in the login information into the fields priviously found
+    print(email_input)
+    print(pass_input)
+    let_load(100)
+    email_input.send_keys(logins.email1)
+    let_load(100)
+    pass_input.send_keys(logins.password1)
+    let_load(100)
+
+shot_quality()
