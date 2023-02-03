c1b1f8a9ef33b5b8a02c74d6062fe29728cdb538
Max Doumitt <grengren34@gmail.com>
2023-02-03 12:11:11 -0500

    Commit 3
    Got the login process completed and got the first screenshot on ShotQuality successfully taken.

--- UTC Basketball/testSelenium.py
+++ UTC Basketball/testSelenium.py
@@ -8,12 +8,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 #Global variables and functions
+#initialize information about the team in question
+team_name = "Chattanooga"
+players = [ #list of player names to gather info on
+    "player0",
+    "player1",
+    "player2",
+    "player3",
+    "player4",
+    "player5",
+    #etc...
+]
+
+
+#Code Begins ↓
+
+
 #Sets Chrome as web browser (Install Chrome & Chromium)
 driver = webdriver.Chrome()
 #Let the page load for a few seconds
 def wait():
     time.sleep(5)
-
 
 #ShotQuality Screenshot Function
 def shot_quality():
@@ -23,6 +38,9 @@ def shot_quality():
     assert "ShotQuality" in driver.title 
     #Load page
     wait()
+    #maximizes browser
+    driver.maximize_window()
+    wait()
     #Find the locations of necessary LOGIN page items
     email_input = driver.find_element(By.XPATH, "//input[@type=\"email\"]") #email field
     pass_input = driver.find_element(By.XPATH, "//input[@type=\"password\"]") #password field
@@ -30,12 +48,25 @@ def shot_quality():
     #writes in the login information into the fields priviously found
     email_input.send_keys(logins.email1)
     pass_input.send_keys(logins.password1)
+    #Clicks Login
     login_button.click()
-    driver.implicitly_wait(15)
-    search_bar_init = driver.find_element(By.XPATH, "//input[@type=\"text\"]") #Search Bar on Initial Page
-    search_bar_init.send_keys("Chattanooga")
     wait()
-    search_bar_init.send_keys(Keys.RETURN)
+    #Selects search bar and types the team name into it
+    search_bar_init = driver.find_element(By.XPATH, "//input[@type=\"text\"]") #Search Bar on Initial Page
+    search_bar_init.send_keys(team_name)
+    wait()
+    #Clicks the FIRST search result when team name is written in
+    content_box = driver.find_element(By.XPATH, "//span[@class=\"search-results__text\"]")
+    content_box.click()
+    wait()
+    #click the team link to open their stats page
+    team_link = driver.find_element(By.LINK_TEXT, team_name)
+    team_link.click()
+    wait()
+    #Screenshot the main team stats
+    team_stats = driver.find_element(By.XPATH, "//ul[@class=\"list-description rankList\"]")
+    team_stats.screenshot(team_name+'_stats.png')
+    #pauses program at end
     time.sleep(111)
 
 
