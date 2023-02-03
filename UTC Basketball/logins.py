9c945e723a3a5806c678aa58c37401b3cf1e09ce
Max Doumitt <grengren34@gmail.com>
2023-02-02 19:34:43 -0500

    commit 2
    at the search bar on the initial page after logging in on ShotQuality.

--- UTC Basketball/logins.py
+++ UTC Basketball/logins.py
@@ -1,5 +1,5 @@
 #storage for emails and passwords
-email1 = "email1"
-email2 = "email2"
-password1 = "password1"
-password2 = "password2"
+email1 = ""
+email2 = ""
+password1 = ""
+password2 = ""
--- UTC Basketball/testSelenium.py
+++ UTC Basketball/testSelenium.py
@@ -1,16 +1,18 @@
 #Import Usernames and Passwords
 import logins
+import time
 #Selenium Imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 #Global variables and functions
 #Sets Chrome as web browser (Install Chrome & Chromium)
 driver = webdriver.Chrome()
 #Let the page load for a few seconds
-def let_load(x):
-    driver.implicitly_wait(x) 
+def wait():
+    time.sleep(5)
 
 
 #ShotQuality Screenshot Function
@@ -20,17 +22,21 @@ def shot_quality():
     #Checks if "ShotQuality" is in the title to varify we are in the right place
     assert "ShotQuality" in driver.title 
     #Load page
-    let_load(150)
+    wait()
     #Find the locations of necessary LOGIN page items
     email_input = driver.find_element(By.XPATH, "//input[@type=\"email\"]") #email field
     pass_input = driver.find_element(By.XPATH, "//input[@type=\"password\"]") #password field
+    login_button = driver.find_element(By.XPATH, "//button[@type=\"submit\"]") #Login Button
     #writes in the login information into the fields priviously found
-    print(email_input)
-    print(pass_input)
-    let_load(100)
     email_input.send_keys(logins.email1)
-    let_load(100)
     pass_input.send_keys(logins.password1)
-    let_load(100)
+    login_button.click()
+    driver.implicitly_wait(15)
+    search_bar_init = driver.find_element(By.XPATH, "//input[@type=\"text\"]") #Search Bar on Initial Page
+    search_bar_init.send_keys("Chattanooga")
+    wait()
+    search_bar_init.send_keys(Keys.RETURN)
+    time.sleep(111)
+
 
 shot_quality()
